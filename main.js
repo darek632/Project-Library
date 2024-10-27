369ff61ac6693ffc8523522058e4394b86739c11
“darek632” <162553961+darek632@users.noreply.github.com>
2024-10-27 13:58:08 +0000

    create function to append object values to card
    Creates a separatre function for:
    adding book to library array
    creating a card with elements and classes to match
    appends text content of each object to specific
    elements on new card

--- main.js
+++ main.js
@@ -1,86 +1,127 @@
 const myLibrary = [];
 
+
+// object constructor function
 function Book(title, author, pages, read) { 
 
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    // this.info = function() { 
-    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    // };
+   
   }
-
-  const book1 = new Book("The Hotel Avocado", "Bob Mortimer", 416 , "not read yet");
-
-
-  function addBookToLibrary() { 
-    let question = prompt("Please enter the title,author,number of pages, read or not yet read, separated by commas");
-    let bookParameters = question.split(",");
-    let newBook = new Book(bookParameters[0], bookParameters[1], bookParameters[2], bookParameters[3]);
-    myLibrary.push(newBook);
-
-    return myLibrary;
-  }
-
-  const book2 = {
-    title: "A Gentleman in Moscow",
-    author: "Amor Towles",
-    pages: 512,
-    read: "read",
-  }
-
-  myLibrary.push(book1,book2); // adding books to store in array
 
 
 let mainDiv = document.querySelector(".main-container");
 
+let card = document.createElement("div");
+
+
+let title = document.createElement("div");
+let authorLabel = document.createElement("div");
+let author = document.createElement("div");
+let pagesLabel = document.createElement("div");
+let pages = document.createElement("div");
+let read = document.createElement("div");
+
+
+function addBookToLibrary() { 
+    let question = prompt("Please enter the title,author,number of pages, read or not yet read, separated by commas", "Harry Potter and the Philosopher's stone, J K Rowling, 352, read");
+    let bookParameters = question.split(",");
+    let newBook = new Book(bookParameters[0], bookParameters[1], bookParameters[2], bookParameters[3]);
+    myLibrary.push(newBook);
+
+  }
 
 // function to loop through each array and display each book on page.
-myLibrary.forEach( (element) => { 
-let sampleBook = "";
-for (let x in element) { 
-    sampleBook += element[x] + " ";
-};
 
-let newBook = document.createElement("div");
 
-newBook.textContent = sampleBook;
-
-mainDiv.appendChild(newBook);
-
-}
-);
 
 // function to create card for each new book
 
-const cardClassNames = ["title", "author-label", "author", "pages-label", "pages", "read"]
+const cardClassNames = ["title", "author-label", "author", "pages-label", "pages", "read"] //array of class names for card to create each div and assign 
+//appropriate class to comply with card styling for each new book
 
-function createCard () { 
-    let card = document.createElement("div");
+function createCard() { 
+
+//creates initial card but does not append until we've looped through myLibrary and added each value to specific text content element
+
     card.classList.add("card");
+    title.classList.add("title");
+    authorLabel.classList.add("author-label");
+    authorLabel.textContent= "Author:";
+    author.classList.add("author");
+    pagesLabel.classList.add("pages-label");
+    pagesLabel.textContent= "Pages:";
+    pages.classList.add("pages");
+    read.classList.add("read");
+
+}
+ 
+
+//     card.classList.add("card");
+
+
+//     cardClassNames.forEach(className => { 
+//         const div = document.createElement("div");
+//         div.classList.add(className);
+//         div.textContent = `this is ${className}`;
+//         card.appendChild(div);
+//     })
+
+//     mainDiv.appendChild(card) ;
+// }
+
     // for (let i=0; i < 7; i++) { 
     //     const div = document.createElement("div");
     //     div.textContent = "Pussy bitch";
     //     card.appendChild(div);
     // }
 
-    cardClassNames.forEach(className => { 
-        const div = document.createElement("div");
-        div.classList.add(className);
-        div.textContent = `this is ${className}`;
-        card.appendChild(div);
-    })
-
-    mainDiv.appendChild(card) ;
-}
-
-createCard();
-
 // let arraySplit = "";
 // for (let x in myLibrary) { 
 //     arraySplit += myLibrary[x] + " ";
 // };
+
+
+function addBookToPage() { 
+
+createCard();
+
+myLibrary.forEach( (element) => { 
+ 
+    let sampleBook = [];
+    for (let x in element) { 
+        sampleBook.push(element[x]);
+        console.log(sampleBook);
+    };
+   
+    title.textContent= sampleBook[0];
+    author.textContent = sampleBook[1];
+    pages.textContent = sampleBook[2];
+    read.textContent = sampleBook[3];
+
+
+    // let newBook = document.createElement("div");
+    
+
+
+    card.appendChild(title);
+    card.appendChild(authorLabel);
+    card.appendChild(author);
+    card.appendChild(pagesLabel);
+    card.appendChild(pages);
+    card.appendChild(read);
+
+
+    mainDiv.appendChild(card);
+
+    }
+    );
+
+};
+
+
 
 
 
